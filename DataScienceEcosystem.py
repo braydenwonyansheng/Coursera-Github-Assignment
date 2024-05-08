6b45d1715c7ab302990a70da41f0a08f86685cd8
Brayden Won <109968076+braydenwonyansheng@users.noreply.github.com>
2024-05-08 22:24:31 +0800

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# <ol>
+#   <li>Python</li>
+#   <li>R</li>
+#   <li>SQL</li>
+# </ol>
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# <ol>
+#   <li>Pandas</li>
+#   <li>Numpy</li>
+#   <li>Scikitlearn</li>
+# </ol>
+
+# | Data Science Tools |
+# | --- |
+# | Anaconda |
+# | Jupyter Notebooks |
+# | Pycharm |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+(3*4) +5
+
+
+# In[2]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+
+200/60
+
+
+# Objectives:
+# 
+# <ul>
+#   <li>Popular languages for Data Science</li>
+#   <li>Commonly used libraries by Data Scientists</li>
+#   <li>Popular Data Science Tools</li>
+# </ul>
+# 
+
+# ## Author
+# 
+# Brayden
+
+# In[ ]:
+
+
+
+
